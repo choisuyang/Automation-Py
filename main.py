0e0b549658be989afeb3524348604fd6bec28789
stc2061 <stc2061@gepplus.co.kr>
2024-07-11 18:39:40 +0900

    edit

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
-<<<<<<< HEAD
+
 import sys
 import os
 import json
